c1b57edba2e5db07d72dd10236bc478edb36c8d6
Mikael Varshovsky <aunixOS@gmail.com>
2023-08-13 21:57:14 -0500

    finally fixed UCAL

--- functions/UCAL.py
+++ functions/UCAL.py
@@ -5,12 +5,12 @@ class ucal:
     def check(username, commandLevel):
         with open("json-files/ucal.json", "r") as file:
             data = json.load(file)
-            if username not in data['people']:
-                data['people'][username] = 0
+            if username not in data.keys():
+                data[username] = 0
                 with open("json-files/ucal.json", "w") as file:
                     json.dump(data, file, indent=4)
             if username in data:
-                level = data['people'][username]
+                level = data[username]
                 if level >= commandLevel:
                     return True
                 else:
@@ -18,14 +18,11 @@ class ucal:
     def raiseLevel(username, levels):
         with open("json-files/ucal.json", "r") as file:
             data = json.load(file)
-            data['people'][username] = data['people'][username] + levels
+            data[username] = data[username] + levels
             with open("json-files/ucal.json", "w") as file:
                 json.dump(data, file, indent=4)
     def add(username):
         with open("json-files/ucal.json") as file:
             data = json.load(file)
-            if username not in data['people']:
-                pass
-            else:
-                data['people'][username] = 0
-            
+            if username not in data.keys():
+                data[username] = 0
