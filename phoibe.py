5c9ff7bb81533acd3c15925cfcbf88b872dcd9ac
Reid Powell <garitbibbs@gmail.com>
2023-09-25 15:04:11 -0500

    added vend

--- phoibe.py
+++ phoibe.py
@@ -502,6 +502,28 @@ while True:
             else:
                 send_message("this command has been disabled")
             internalFunctions.logs.writeToLogs("INFO - " + username + " used a command")
+# vend
+        if ".vend" in message:
+            if settings.funcSettings.useVend is True:
+                if settings.funcSettings.useUCAL is True:
+                    if UCAL.ucal.check(username, settings.ucalLevels.vend) is True:
+                        try:
+                            com, user = message.split(" ")
+                        except ValueError:
+                            user = username
+                        send_message(Frandom.vend.vend(username))
+                    else:
+                        send_message("your ucal level is not high enough to use this command")
+                else:
+                    try:
+                        com, user = message.split(" ")
+                    except ValueError:
+                        user = username
+                    send_message(Frandom.vend.vend(username))
+            else:
+                send_message("this room has vend disabled")
+            internalFunctions.logs.writeToLogs("INFO - " + username + " used a command")
+
 # wew
         if ".wew" in message:
             if settings.funcSettings.useWew is True:
